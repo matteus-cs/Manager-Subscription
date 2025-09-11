dcb09158b20226263f1beacaaa6ee6bac376f438
Matteus Santos <matteus.py@gmail.com>
2025-09-11 20:07:55 -0300

    feat: add DTO validation for customer creation

---
+++ Api/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,41 @@
+using Api.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Dtos;
+
+public record CreateCustomerDto
+(
+    [Required]
+    [StringLength(50, MinimumLength = 2)]
+    string Name,
+
+    [Required]
+    [EmailAddress]
+    string Email,
+
+    [Required]
+    [StringLength(14, MinimumLength = 11)]
+    string Cpf,
+
+    [Required]
+    [RegularExpression(@"^(?:\+55\s?)?\(?\d{2}\)?\s?(?:9\d{4}|\d{4})-?\d{4}$", ErrorMessage = "Invalid phone format")]
+    string Phone,
+
+    [Required]
+    CreateAddressDto Address
+);
+
+public record CreateAddressDto
+(
+    [Required]
+    string Street,
+
+    [Required]
+    string Neighborhood,
+
+    [Required]
+    string City,
+
+    [Required]
+    string State
+);
