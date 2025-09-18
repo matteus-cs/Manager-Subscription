e7b4998d4291f81766008e644d87a0e1b2070572
Matteus Santos <matteus.py@gmail.com>
2025-09-17 21:43:07 -0300

    feat: add subscription entity

---
+++ Api/Entities/Subscription.cs
@@ -0,0 +1,22 @@
+namespace Api.Entities;
+
+public class Subscription
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+
+    public DateOnly StartDate { get; set; }
+
+    public DateOnly EndDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public byte DueDate { get; set; }
+
+    public Customer Customer { get; set; } = default!;
+
+    public Guid CustomerId { get; set; }
+
+    public Plan Plan { get; set; } = default!;
+
+    public Guid PlanId { get; set; }
+}
